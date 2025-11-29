e2fbf3e0f77027b6457c79bb99ff95137b57469e
aboualam <abdellah.boualam@etu.univ-cotedazur.fr>
2025-11-30 00:37:10 +0100

    dernier version

--- assignment-app/src/server.js
+++ assignment-app/src/server.js
@@ -1,19 +1,23 @@
-
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-
+// Build Angular (dist)
 const distFolder = path.join(__dirname, 'dist', 'assignment-app', 'browser');
 
-app.use(express.static(distFolder));
+// Fichiers statiques
+app.use(express.static(distFolder, {
+  maxAge: '1y',
+  index: false
+}));
 
-app.get('*', (req, res) => {
+// Catch-all route (Express 5 compatible)
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(distFolder, 'index.html'));
 });
 
+// Lancement serveur
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Front Angular servi sur http://localhost:${port}`);
